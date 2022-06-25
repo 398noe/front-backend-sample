9ab387fe418556c6603d52133abcad498fa7ef43
Sakyuba Noe <398noe@gmail.com>
2022-06-25 19:11:12 +0900

    Add type definition

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,6 +1,12 @@
-export interface User {
-    id: number;
+export interface UserData  {
+    id?: string;
     name: string;
     age: number;
     email: string;
+}
+
+export interface Message {
+    data: any;
+    status: string;
+    date: string;
 }
