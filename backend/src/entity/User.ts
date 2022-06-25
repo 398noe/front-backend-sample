e19e3ca19fc7a1f9725e30c82dfbff347518f9a7
Sakyuba Noe <398noe@gmail.com>
2022-06-25 19:10:58 +0900

    Add user entity

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -2,20 +2,21 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
 export class User extends BaseEntity {
-    @PrimaryGeneratedColumn()
-        readonly id!: number;
+    @PrimaryGeneratedColumn("increment")
+        readonly id?: string;
 
     @Column({length: 20, nullable: false})
-        private name!: string;
+        private name?: string;
 
     @Column({length: 3, nullable: false})
-        private age!: number;
+        private age?: number;
 
     @Column({length: 50, nullable: false})
-        private email!: string;
+        private email?: string;
 
-    constructor(name: string, age: number, email:string) {
+    constructor(id: string, name: string, age: number, email:string) {
         super();
+        this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
