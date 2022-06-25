3bd2c95545bc05d2f060d54035fe759909a7f007
Sakyuba Noe <398noe@gmail.com>
2022-06-25 21:43:13 +0900

    Add seeder on migration

--- backend/src/migration/1656157576122-UserMigration.ts
+++ backend/src/migration/1656157576122-UserMigration.ts
@@ -1,4 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
+import { UserSeed } from "../seed/user.seed";
+import { db } from "../data-source";
+import { User } from "../entity/User";
 
 export class UserMigration1656157576122 implements MigrationInterface {
 
@@ -9,7 +12,11 @@ export class UserMigration1656157576122 implements MigrationInterface {
             age number,
             email string NOT NULL,
             created TIMESTAMP DEFAULT (datetime(CURRENT_TIMESTAMP,'localtime')))
-            `);
+        `);
+        // Get seed data and insert for initial data
+        const userSeed = UserSeed;
+        const userRepository = db.getRepository(User);
+        userRepository.insert(userSeed);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
---
+++ backend/src/seed/user.seed.ts
@@ -0,0 +1,11 @@
+
+export const UserSeed = [
+    {
+        id: "01G6DCK9Z8YQQG3870NV3XHK0J",
+        name: "Test",
+        age: 20,
+        email: "test@example.com", 
+        createdAt: `${new Date()}`,
+        updatedAt: `${new Date()}`
+    }
+]
