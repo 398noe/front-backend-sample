e213632f20c1c3b0b5e477f7e3a8a13028d94c5b
Sakyuba Noe <398noe@gmail.com>
2022-06-26 19:02:07 +0900

    Add react hook form

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,10 +1,11 @@
-import { Flex, Box, FormControl, FormLabel, Input, Stack, Button, useColorModeValue, Alert, AlertIcon, AlertTitle } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, Stack, Button, useColorModeValue, Alert, AlertIcon, AlertTitle, Center, Heading } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import axios from 'axios';
 import aspida from "@aspida/axios";
 import api from "../api/$api";
 import { userDataState } from '../atom/userData';
+import { useForm } from 'react-hook-form';
 
 
 const Login: React.FC = () => {
@@ -16,7 +17,15 @@ const Login: React.FC = () => {
     }
     // aspida
     const client = api(aspida(axios, { baseURL: "http://localhost:8000/api" }));
-
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        defaultValues: {
+            id: userData.id,
+        }
+    });
     const getUserData = async () => {
         // idからデータを取得
         console.log(id);
@@ -31,45 +40,49 @@ const Login: React.FC = () => {
         }
     }
     return (
-        <Flex
-            align={'center'}
-            justify={'center'}
-            bg={useColorModeValue('gray.50', 'gray.800')}>
-            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-                <Box
-                    rounded={'lg'}
-                    bg={useColorModeValue('white', 'gray.700')}
-                    boxShadow={'lg'}
-                    p={8}>
+        <Center py={6}>
+            <Box
+                maxW={"270px"}
+                w={"270px"}
+                overflow={"hidden"}
+                rounded={'lg'}
+                bg={useColorModeValue('white', 'gray.700')}
+                boxShadow={'lg'}
+                p={8}>
+                <Stack spacing={4}>
+                    <Heading fontSize={'2xl'} textAlign={'center'}>
+                        ログイン
+                    </Heading>
+                    {
+                        isError ? (
+                            <Alert status='error' maxW={"270px"}>
+                                <AlertIcon />
+                                <AlertTitle>IDが正しくありません</AlertTitle>
+                            </Alert>
+                        ) : (<></>)
+                    }
+                    <Input isRequired={true} variant="flushed" placeholder="ID"
+                        {...register("id", { required: true })}
+                        value={id}
+                        onChange={handleId}
+                    />
                     <Stack spacing={4}>
-                        {
-                            isError ? (
-                                <Alert status='error' maxW={"270px"}>
-                                    <AlertIcon />
-                                    <AlertTitle>IDが正しくありません</AlertTitle>
-                                </Alert>
-                            ) : (<></>)
-                        }
-                        <FormControl id="email">
-                            <FormLabel>IDを入力</FormLabel>
-                            <Input type="text" onChange={handleId} placeholder={"ID"} defaultValue={id} />
-                        </FormControl>
-                        <Stack spacing={4}>
-                            <Button
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}
-                                onClick={getUserData}
-                            >
-                                データを取得
-                            </Button>
-                        </Stack>
+                        <Button
+                            bg={'blue.400'}
+                            color={'white'}
+                            _hover={{
+                                bg: 'blue.500',
+                            }}
+                            onClick={handleSubmit((data) => {
+                                getUserData();
+                            })}
+                        >
+                            ログイン
+                        </Button>
                     </Stack>
-                </Box>
-            </Stack>
-        </Flex>
+                </Stack>
+            </Box>
+        </Center>
     );
 }
 
